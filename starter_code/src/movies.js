fe620b6c19eea9a3b8432c0ed326f0b82a881345
Omar Caraballo <ocaraballo21061@columbushs.com>
2019-11-24 13:34:16 -0500

    done

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -92,7 +92,7 @@ function dramaMoviesRate(array){
     return eachMovie.genre == "Drama";
   })
   return ratesAverage(dramaMovies)
-}
+};
 
 // Iteration 6: Time Format - Turn duration of the movies from hours to minutes
 
@@ -114,14 +114,57 @@ function dramaMoviesRate(array){
     // let hour = 
 
 
-
-
-
-
-
-
-
+    function hoursToMinutes (duration) {
+      var minDuration = 0;
+      var hours = 0;
+      var mins = 0;
+      var geth = duration.indexOf("h");
+      var getmin = duration.indexOf("min");
+      
+      if (geth != -1 && getmin != -1 && duration[getmin-2] !== " ") {
+        hours = parseInt(duration[geth-1]);
+        mins = parseInt(duration[getmin-1]) + parseInt(duration[getmin-2]) * 10;
+      }
+      else if (geth != -1 && getmin != -1 && duration[getmin-2] === " ") {
+        hours = parseInt(duration[geth-1]);
+        mins = parseInt(duration[getmin-1]);
+      }
+      else if (geth == -1 && getmin != -1 && duration[getmin-2] === " ") {
+        mins = parseInt(duration[getmin-1]);
+      }
+      else if (geth == -1 && getmin != -1 && duration[getmin-2] !== " ") {
+        mins = parseInt(duration[getmin-1]) + parseInt(duration[getmin-2]) * 10;
+      }
+      else if (geth != -1 && getmin == -1) {
+        hours = parseInt(duration[geth-1]);
+      }
+      
+      minDuration = hours * 60 + mins;
+      return minDuration;
+    }
     
+    function turnHoursToMinutes (mov) {
+      const minutesMovies = mov.map( movie => {
+        const minduration = hoursToMinutes (movie.duration);
+        return {
+          director: movie.director,
+          duration: minduration,
+          genre: movie.genre,
+          rate: movie.rate,
+          title: movie.title,
+          year: movie.year
+        }
+      })
+      return minutesMovies;
+    }
+
+
+
+
+
+
+
+
     // function turnHoursToMinutes(arr){
     //   let newArr = arr.map(a => Object.assign({}, a));
     //   for(let i = 0; i < newArr.length; i += 1){
