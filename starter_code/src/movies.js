1c730ffd9e1edd51ee6796d89f2a0b4cdfd0ea31
Omar Caraballo <ocaraballo21061@columbushs.com>
2019-11-24 13:03:49 -0500

    Done?

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -121,6 +121,40 @@ function dramaMoviesRate(array){
 
 
 
+    
+    // function turnHoursToMinutes(arr){
+    //   let newArr = arr.map(a => Object.assign({}, a));
+    //   for(let i = 0; i < newArr.length; i += 1){
+    //     if(typeof(newArr[i].duration) !== Number){
+    //       if(newArr[i].duration.indexOf(' ') !== -1 && newArr[i].duration.indexOf('m') !== -1){
+    //         let hoursMinutes = newArr[i].duration.split(' ');      
+    //         let hours = (parseInt(hoursMinutes[0].split(''))) * 60;
+    //         let minutes = parseInt(hoursMinutes[1].split('m'));
+    //         newArr[i].duration = hours + minutes;
+    //       }else if(newArr[i].duration.indexOf('h') !== -1){
+    //         let hoursMinutes = newArr[i].duration.split(' ');
+    //         let hours = (parseInt(hoursMinutes[0].split(''))) * 60;
+    //         newArr[i].duration = hours;
+            
+    //       }else if(newArr[i].duration.indexOf('m') !== -1){
+    //         let hoursMinutes = newArr[i].duration.split(' ');
+    //         let minutes = (parseInt(hoursMinutes));
+    //         newArr[i].duration = minutes;
+    //       }
+    //     }else{
+    //         continue;
+    //       }
+    //   }
+    // return (newArr);  
+    // };
+
+
+
+
+
+
+
+
 
 
 
